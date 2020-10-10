8ceb83edf13e9cf930b8f6a32dec12881e05d3d7
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2020-10-09 23:53:03 -0600

    updated weather

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '6cd7e2f416dc1bbd8ee3a954d9fa55f2';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 43.649811;
+    let longitude = -116.281342;
 
     getWeather(latitude, longitude);
 }
